bde9fbfc5888b74260f9e3df0f3a350b26bceea0
H-Gerst <hannah.gerstmyer@gmail.com>
2025-06-02 20:34:38 +1000

    Update 1_SMILES_setup.py

--- 1_SMILES_setup.py
+++ 1_SMILES_setup.py
@@ -1,18 +1,12 @@
-
 import warnings
 warnings.filterwarnings('ignore', category=DeprecationWarning)
 
-from rdkit import Chem, DataStructs
+from rdkit import Chem, DataStructs, RDLogger
 from rdkit.Chem import Mol, PandasTools, AllChem, Draw, rdMolDescriptors
-# from rdkit.Chem import MACCSkeys
-from rdkit.Chem.rdFingerprintGenerator import GetMorganGenerator
-import sklearn
-from sklearn.feature_extraction import DictVectorizer
-import numpy as np
 import cirpy
 import pandas as pd
 
-def kill_yourself(df):
+def cas_to_smiles(df):
     cas_list = df['CAS'].tolist()
     bucket = []
     i = 0
@@ -20,30 +14,79 @@ def kill_yourself(df):
         try:
             fuckit = cirpy.resolve(cas_list[i], 'smiles')
             bucket.append(fuckit)
+            print(i/len(cas_list)*100)
         except Exception as e:
-            print(i)
-            print('well aren\'t you stooopid')
+            print(i, 'well aren\'t you stooopid')
         i += 1
     print(bucket)
     df['SMILES_NEW'] = bucket
     df['SMILES_NEW'] = df['SMILES_NEW'].astype("str")
     return df
 
+def ring_sys_count(df):
+    mol_list = df['SMILES_NEW'].tolist()
+    ring_bucket = []
+    i = 0
+    while i < len(mol_list):
+        try:
+            ring_col = cirpy.resolve(mol_list[i], 'ringsys_count')
+            ring_bucket.append(ring_col)
+            print(i / len(mol_list) * 100)
+        except Exception as e:
+            print(i, 'well aren\'t you stooopid')
+            ring_bucket.append('error here')
+        i += 1
+    print(ring_bucket)
+    df['RING_SYS'] = ring_bucket
+    return df
+
+def clean_and_check(data, kind):
+    PandasTools.AddMoleculeColumnToFrame(data, smilesCol='SMILES_NEW', molCol='MOLECULE', includeFingerprints=False)
+    err = data[data['MOLECULE'].isna()].index.tolist()
+    print('There are this many bad molecules in the training data:', len(err))
+    clean_data = data.dropna(subset=['MOLECULE'])
+    clean_err = data[data['MOLECULE'].isna()].index.tolist()
+
+    if len(clean_err) > len(err):
+        print('... Something is still wrong')
+    else:
+        print('Nice! the', kind, 'data is okay')
+
+    print(clean_data.columns.to_list())
+    print('Here is the cleaned', kind, 'data: \n', clean_data)
+
+    return clean_data
+
 def main():
-    ames_data = pd.read_excel("New_data.xlsx")
-    kill_yourself(ames_data)
-    print(ames_data)
+    # Ignoring 'WARNING: Not removing hydrogen without neighbours'
+    RDLogger.DisableLog('rdApp.*')
 
-    PandasTools.AddMoleculeColumnToFrame(ames_data, smilesCol='SMILES_NEW', molCol='MOLECULE', includeFingerprints=False)
-    next_mol_err = ames_data[ames_data['MOLECULE'].isna()].index.tolist()
-    print(len(next_mol_err))
+    # TRAINING DATA
+    # Loading data for model training
+    training_data = pd.read_csv("ames_mutagenicity_data_training.csv")
 
-    ames_data = ames_data.dropna(subset=['MOLECULE'])
-    next_mol_err = ames_data[ames_data['MOLECULE'].isna()].index.tolist()
+    # Checking that everything is okay so far
+    print('Here is the initial training data: \n', training_data)
+    cleaned_training_data = clean_and_check(training_data, kind='training')
 
-    print(len(next_mol_err))
+    # Saving to csv
+    cleaned_training_data.to_csv("clean_training_data.csv", index=False)
 
-    ames_data.to_csv("ames_w_smiles_external.csv", index=False)
+
+    # VALIDATION DATA
+    # Loading the validation dataset
+    validation_ames_data = pd.read_excel("new_data.xlsx")
+
+    # Checking that everything is as expected
+    print('Here is the initial validation data: \n', validation_ames_data)
+
+    # Adding a molecule column from smiles
+    cas_to_smiles(validation_ames_data)
+    cleaned_validation_data = clean_and_check(validation_ames_data, kind='validation')
+
+    # Saving the dataframe as csv
+    cleaned_validation_data.to_csv("clean_validation_data.csv", index=False)
+
 
 if __name__ == "__main__":
     main()
